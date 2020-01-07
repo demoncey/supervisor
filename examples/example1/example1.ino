862f41268e2f331e6573cf03d3e2c895067b6e83
demoncey <demoncey@gmail.com>
2020-01-07 23:17:48 +0100

    extending example

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -1,16 +1,19 @@
 #include <task.h>
 #include <supervisor.h>
 void send_msg(void) ;
+void send_msg2(void) ;
 
 Supervisor supervisor("example1 supervisor");
 Task task1(send_msg);
+Task task2(send_msg2);
 
 void setup() {
   Serial.begin(9600);
     while (!Serial) {
     ; 
   }
-  supervisor.addTask(task1.setPriority(P_HIGH));
+  supervisor.addTask(*task1.setPriority(P_HIGH));
+  supervisor = supervisor+ task2;
 }
 
 void loop() {
@@ -21,5 +24,11 @@ void loop() {
 
 void send_msg(void*) {
   int id = random(1,65536);
-  Serial.println("Heartbeat msg: "+String(id,HEX));
+  Serial.println("Heartbeat msg YYYYYYYYYYYYYYYYY: "+String(id,HEX));
+}
+
+
+void send_msg2(void*) {
+  int id = random(1,65536);
+  Serial.println("Heartbeat msg YYYYYYYYYYYYYYYYY: "+String(id,HEX));
 }
