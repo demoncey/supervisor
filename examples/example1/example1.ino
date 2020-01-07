f4a9155a767c329f46d67bfbaf92b4db154f2b93
demoncey <demoncey@gmail.com>
2020-01-07 23:08:19 +0100

    example

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -1,17 +1,25 @@
 #include <task.h>
 #include <supervisor.h>
-
+void send_msg(void) ;
 
 Supervisor supervisor("example1 supervisor");
-
-
+Task task1(send_msg);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+    while (!Serial) {
+    ; 
+  }
+  supervisor.addTask(task1.setPriority(P_HIGH));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    supervisor.execute();
+}
 
+
+void send_msg(void*) {
+  int id = random(1,65536);
+  Serial.println("Heartbeat msg: "+String(id,HEX));
 }
